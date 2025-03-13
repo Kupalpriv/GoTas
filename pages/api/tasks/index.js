79e79993bd1c84e91fa1df02a9c8975069ace935
Kupalpriv <churchillabing100@gmail.com>
2025-03-13 22:13:35 +0800

    Create index.js

---
+++ pages/api/tasks/index.js
@@ -0,0 +1,17 @@
+import connectDB from '../../../lib/mongodb';
+import Task from '../../../models/Task';
+
+export default async function handler(req, res) {
+  await connectDB();
+
+  if (req.method === 'GET') {
+    const tasks = await Task.find();
+    return res.status(200).json(tasks);
+  }
+
+  if (req.method === 'POST') {
+    const newTask = new Task(req.body);
+    await newTask.save();
+    return res.status(201).json(newTask);
+  }
+}
