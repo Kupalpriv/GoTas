415f07190690e4fc28bce5726122b0edef38f64e
Kupalpriv <churchillabing100@gmail.com>
2025-03-13 22:12:54 +0800

    Create [id].js

---
+++ pages/api/tasks/[id].js
@@ -0,0 +1,18 @@
+port connectDB from '../../../lib/mongodb';
+import Task from '../../../models/Task';
+
+export default async function handler(req, res) {
+  await connectDB();
+
+  const { id } = req.query;
+
+  if (req.method === 'DELETE') {
+    await Task.findByIdAndDelete(id);
+    return res.status(200).json({ message: 'Task deleted' });
+  }
+
+  if (req.method === 'PUT') {
+    const updatedTask = await Task.findByIdAndUpdate(id, req.body, { new: true });
+    return res.status(200).json(updatedTask);
+  }
+}
