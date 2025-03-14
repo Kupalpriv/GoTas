816b4c3f992692e12bd8e3859b71cef219f6bbea
Kupalpriv <churchillabing100@gmail.com>
2025-03-14 10:04:40 +0800

    Update index.js

--- pages/api/tasks/index.js
+++ pages/api/tasks/index.js
@@ -2,16 +2,18 @@ import connectDB from '../../../lib/mongodb';
 import Task from '../../../models/Task';
 
 export default async function handler(req, res) {
-  await connectDB();
+    await connectDB(); // Connect to MongoDB
 
-  if (req.method === 'GET') {
-    const tasks = await Task.find();
-    return res.status(200).json(tasks);
-  }
+    if (req.method === 'GET') {
+        const tasks = await Task.find();
+        return res.status(200).json(tasks);
+    } 
+    
+    if (req.method === 'POST') {
+        const newTask = new Task(req.body);
+        await newTask.save();
+        return res.status(201).json(newTask);
+    }
 
-  if (req.method === 'POST') {
-    const newTask = new Task(req.body);
-    await newTask.save();
-    return res.status(201).json(newTask);
-  }
+    return res.status(405).json({ message: 'Method Not Allowed' });
 }
